f74d20975be8a21e4653d69ab78e663d886b0a31
yu-zhen <wlphg010688@gmail.com>
2023-03-20 16:37:25 +0700

    feat: add logout function and button

--- packages/frontend/src/layout/Header.jsx
+++ packages/frontend/src/layout/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Header, Button, Segment } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 
@@ -8,6 +8,12 @@ import User from "../contexts/User";
 export default observer(() => {
   const location = useLocation();
   const user = useContext(User);
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await user.logout();
+    return navigate("/");
+  };
 
   return (
     <Segment clearing raised>
@@ -21,10 +27,15 @@ export default observer(() => {
         )}
         {location.pathname !== "/user" && user.hasSignedUp && (
           <Link to="/user">
-            <Button color="purple" size="large" basic>
+            <Button color="purple" size="large">
               My Page
             </Button>
           </Link>
+        )}
+        {user.hasSignedUp && (
+          <Button color="orange" size="large" basic onClick={logout}>
+            Logout
+          </Button>
         )}
       </Header>
       <Link to="/">
