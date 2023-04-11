258b5349d5da784ba92f5c3b959f5fa8408ac405
yu-zhen <wlphg010688@gmail.com>
2023-04-11 18:17:43 +0900

    contracts: add verifier contract

--- packages/contracts/scripts/abi.ts
+++ packages/contracts/scripts/abi.ts
@@ -1,8 +1,14 @@
 import fs from "fs";
 import path from "path";
 import UNIREPAPP_ABI from "../artifacts/contracts/UnirepApp.sol/UnirepApp.json";
+import Verifier_ABI from "../artifacts/contracts/ProveDataVerifier.sol/ProveDataVerifier.json";
 
 fs.writeFileSync(
   path.join(__dirname, "../abi/UnirepApp.json"),
   JSON.stringify(UNIREPAPP_ABI)
+);
+
+fs.writeFileSync(
+  path.join(__dirname, "../abi/ProveDataVerifier.json"),
+  JSON.stringify(Verifier_ABI)
 );
