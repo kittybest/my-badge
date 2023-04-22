2ad5715c6d3a10f750381aa7fc14b1337c8a7889
yu-zhen <wlphg010688@gmail.com>
2023-04-22 17:49:48 +0900

    relay: new post ranking data router

---
+++ packages/relay/src/routes/ranking.ts
@@ -0,0 +1,69 @@
+import { Express } from "express";
+import { ethers } from "ethers";
+import { DB } from "anondb/node";
+import { Synchronizer } from "@unirep/core";
+import { DataProof } from "@unirep-app/circuits";
+import { UNIREP_ADDRESS, provider } from "../config";
+import TransactionManager from "../singletons/TransactionManager";
+
+import UNIREP_ABI from "@unirep/contracts/artifacts/contracts/Unirep.sol/Unirep.json";
+import UNIREPAPP_ABI from "@unirep-app/contracts/artifacts/contracts/UnirepApp.sol/UnirepApp.json";
+
+export default (app: Express, db: DB, synchronizer: Synchronizer) => {
+  // upload data proof and publicSignals
+  app.post("/api/ranking", async (req, res) => {
+    try {
+      const { publicSignals, proof, attesterId, currentData } = req.body;
+      console.log("upload ranking data:", publicSignals, currentData);
+
+      // make data proof
+      const dataProof: DataProof = new DataProof(publicSignals, proof);
+
+      // check attesterId
+      if (dataProof.attesterId.toString() !== BigInt(attesterId).toString()) {
+        res.status(400).json({ error: "Attester Id does not match." });
+        return;
+      }
+
+      // verify data proof
+      const valid = await dataProof.verify();
+      if (!valid) {
+        res.status(400).json({ error: "Invalid proof" });
+        return;
+      }
+
+      // check epoch matches or not
+      const unirepContract = new ethers.Contract(
+        UNIREP_ADDRESS,
+        UNIREP_ABI.abi,
+        provider
+      );
+      const epoch = Number(
+        await unirepContract.attesterCurrentEpoch(attesterId)
+      );
+      if (Number(dataProof.epoch) !== epoch) {
+        res.status(400).json({
+          error: `Wrong epoch: should be ${epoch}, but it is ${Number(
+            dataProof.epoch
+          )} in the proof.`,
+        });
+        return;
+      }
+
+      // send data on chain
+      const appContract = new ethers.Contract(attesterId, UNIREPAPP_ABI.abi);
+      let calldata = appContract.interface.encodeFunctionData(
+        "submitDataProof",
+        [dataProof.publicSignals, dataProof.proof]
+      );
+      const hash = await TransactionManager.queueTransaction(
+        attesterId,
+        calldata
+      );
+      res.json({ hash });
+    } catch (error: any) {
+      console.log("post ranking data error:", error);
+      res.status(500).json({ error });
+    }
+  });
+};
